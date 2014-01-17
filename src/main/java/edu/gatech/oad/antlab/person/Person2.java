b19eca43050bdf652247083eab22f78599206a96
sand500 <sand500@gmail.com>
2014-01-16 19:02:22 -0500

    Added stuff
    Added stuff to person2.java

--- src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -1,25 +1,26 @@
 package edu.gatech.oad.antlab.person;
+import java.util.Random;
 
 /**
  *  A simple class for person 2
  *  returns their name and a
  *  modified string 
  *
- * @author Bob
+ * @author Bob, Sandeep
  * @version 1.1
  */
 public class Person2 {
-    /** Holds the persons real name */
-    private String name;
+	/** Holds the persons real name */
+	private String name;
 	 	/**
 	 * The constructor, takes in the persons
 	 * name
 	 * @param pname the person's real name
 	 */
-	 public Person2(String pname) {
-	   name = pname;
-	 }
-	/**
+	 	public Person2(String pname) {
+	 		name = pname;
+	 	}
+	/** 
 	 * This method should take the string
 	 * input and return its characters in
 	 * random order.
@@ -30,8 +31,26 @@ public class Person2 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 2 put your implementation here
-	  return null;
+	 //Person 2 put your implementation here
+		String randString = "";
+
+		int length = input.length();
+		Random randGen = new Random();
+
+		char[] charArray= input.toCharArray();
+		int chars = length;
+		while(chars > 0) {
+
+			int x =0;
+			do {
+				x = randGen.nextInt(length);
+			} while(charArray[x] != '\0');
+			randString += charArray[x];
+			charArray[x] = '\0';
+			chars--; 
+		}
+
+		return randString;
 	}
 	/**
 	 * Return a string rep of this object
@@ -42,6 +61,6 @@ public class Person2 {
 	 *         object
 	 */
 	public String toString(String input) {
-	  return name + calc(input);
+		return name + calc(input);
 	}
 }
